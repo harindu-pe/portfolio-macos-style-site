02f774c649b25eb7d25c576b9bbdbc69348720c1
harindu-pe <harindu.pe@gmail.com>
2026-04-19 22:48:17 +0530

    navbar completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
+import Navbar from '#components/Navbar.jsx';
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold ">Hello World!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 export default App;
